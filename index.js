3d9d0cb12e3ebc57a5f7112c6da6b33bc724e307
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-08 16:18:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use('/api/bugs', bugRoutes)
 
 app.get('/', (req, res) => {
   res.send('🚀 Bug Dashboard Backend is Running!')
+app.get('/api', (req, res) => {
+  res.send("🚀 Bug Dashboard API is live!");
+});
+
 })
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -28,3 +32,4 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`)
 })
+
