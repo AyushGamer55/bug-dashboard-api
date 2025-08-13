b5152e604ddd1c1201c8c782b62ef7c5d0d3fc8b
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-13 16:08:45 +0530

    Update Bug.js

--- models/Bug.js
+++ models/Bug.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const bugSchema = new mongoose.Schema({
-  ScenarioID: String,
+  ScenarioType: String,
   TestCaseID: String,
   Description: String,
   Status: String,
@@ -16,3 +16,4 @@ const bugSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 module.exports = mongoose.model('Bug', bugSchema)
+
