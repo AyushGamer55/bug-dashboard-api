f99e63a17397ac33264e5d7ddc3202d4885b83ed
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-26 14:18:24 +0530

    Update Bug.js

--- models/Bug.js
+++ models/Bug.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 
 const bugSchema = new mongoose.Schema({
-  ScenarioID: { type: String, required: true },
-  TestCaseID: { type: String, required: true },
-  Description: { type: String, required: true },
-  Status: { type: String, required: true },
-  Priority: { type: String, required: true },
-  Severity: { type: String, required: true },
-  PreCondition: { type: String },
-  StepsToExecute: { type: [String] }, 
-  ExpectedResult: { type: String },
-  ActualResult: { type: String },
-  Comments: { type: String },
-  SuggestionToFix: { type: String }
+  ScenarioID: String,
+  TestCaseID: String,
+  Description: String,
+  Status: String,
+  Priority: String,
+  Severity: String,
+  PreCondition: String,
+  StepsToExecute: [String],   
+  ExpectedResult: String,
+  ActualResult: String,
+  Comments: String,
+  SuggestionToFix: String
 }, { timestamps: true })
 
 module.exports = mongoose.model('Bug', bugSchema)
