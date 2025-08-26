7aa78afa8c5946d454b63e13f2b8d8d91d4fabeb
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-26 13:18:27 +0530

    Update Bug.js

--- models/Bug.js
+++ models/Bug.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const bugSchema = new mongoose.Schema({
-  deviceId: { type: String, required: true },
-  ScenarioID: String,
-  TestCaseID: String,
-  Description: String,
-  Status: String,
-  Priority: String,
-  Severity: String,
-  PreCondition: String,
-  StepsToExecute: String,
-  ExpectedResult: String,
-  ActualResult: String,
-  Comments: String,
-  SuggestionToFix: String
-}, { timestamps: true })
+const bugSchema = new mongoose.Schema(
+  {
+    ScenarioID: { type: String },
+    TestCaseID: { type: String },
+    Description: { type: String },
+    Status: { type: String },             
+    Priority: { type: String },           
+    Severity: { type: String },           
+    PreCondition: { type: String },       
+    StepsToExecute: { type: String },     
+    ExpectedResult: { type: String },     
+    ActualResult: { type: String },       
+    Comments: { type: String },          
+    SuggestionToFix: { type: String }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Bug', bugSchema)
-
+module.exports = mongoose.model('Bug', bugSchema);
